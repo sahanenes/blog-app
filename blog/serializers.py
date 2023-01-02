fb5dd34685ef3982c066c9ac64bcf6cd5d28d85b
sahanenes <sahanenesss@gmail.com>
2023-01-02 22:15:43 +0300

    new

---
+++ blog/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Category,Blog
+
+class CategorySerializer(serializers.Serializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id','name'
+        )
+
+class BlogSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    category_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Blog
+        fields = (
+            'id',
+            'title',
+            'content',
+            'category_id',
+            'category',
+            'is_published',
+            'created_date',
+        )
